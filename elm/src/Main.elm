75fb4e48edb9f6be829ee5e41e1a406dbd490634
Michael <eblakem@gmail.com>
2019-11-26 15:16:39 +1000

    Add Elm-UI

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,7 +1,30 @@
 module Main exposing (main)
 
-import Browser
-import Html exposing (h1, text)
+import Element exposing (Element, el, text, row, alignRight, fill, width, rgb255, spacing, centerY, padding)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 
-main =
-  h1 [] [ text "Hello, Elm!" ]
+
+main = 
+    Element.layout []
+        myRowOfStuff
+
+myRowOfStuff : Element msg
+myRowOfStuff =
+    row [ width fill, centerY, spacing 30 ]
+        [ myElement
+        , myElement
+        , el [ alignRight ] myElement
+        ]
+
+
+myElement : Element msg
+myElement =
+    el
+        [ Background.color (rgb255 240 0 245)
+        , Font.color (rgb255 255 255 255)
+        , Border.rounded 3
+        , padding 30
+        ]
+        (text "stylish!")
